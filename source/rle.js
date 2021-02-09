0e59b1454ae11190180955c32e574b03f7c8e57e
Алексей <zotov.alexei.1@yandex.ru>
2021-02-09 12:25:30 +0300

    add helper function

--- source/rle.js
+++ source/rle.js
@@ -17,12 +17,29 @@ const rle = (string) => {
     let compressed = '';
     for (let i = 0; i < string.length; ++i) {
         const currentChar = string.charAt(i);
-        let count = 1;
-        while (i + 1 < string.length && currentChar === string.charAt(i + 1)) {
-            ++count;
-            ++i;
-        }
+        const count = countRecurringChars(string, currentChar, i);
+        i += count - 1;
         compressed += (count === 1) ? currentChar : currentChar + count;
     }
     return compressed;
+}
+
+/**
+ *
+ * @param {string} string - given string
+ * @param {string} char - char, occurrences of which the function counts
+ * @param {number} startIndex - an index, from which the search begins
+ * @returns {number} count - char occurrences in succession
+ *
+ * @ example
+ * // returns 3
+ * countRecurringChars('AAAB', 'A', 0);
+ */
+const countRecurringChars = (string, char, startIndex) => {
+    let count = 1;
+    while (startIndex + 1 < string.length && char === string.charAt(startIndex + 1)) {
+        ++count;
+        ++startIndex;
+    }
+    return count;
 }
